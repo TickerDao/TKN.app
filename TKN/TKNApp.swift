8dacceacf02ea35eb05d6ca6827356bd08cb7dad
Doplur <doplur@protonmail.com>
2023-04-06 16:20:27 -0500

    Three Tab Layout

---
+++ TKN/TKNApp.swift
@@ -0,0 +1,17 @@
+//
+//  TKNApp.swift
+//  TKN
+//
+//  Created by Oak on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct TKNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
