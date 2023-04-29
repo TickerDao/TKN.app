10993c7253298dc9a250dfc054b7035624fa60e6
Doplur <doplur@protonmail.com>
2023-04-29 05:53:06 -0500

    Browse, search, edit, and favorite tab views.

--- TKN/ContentView.swift
+++ TKN/ContentView.swift
@@ -1,4 +1,3 @@
-//
 //  ContentView.swift
 //  TKN
 //
@@ -6,37 +5,107 @@
 //
 
 import SwiftUI
+import SVGKit
 
 struct ContentView: View {
+    @State private var selectedTab = 0
+    @ObservedObject var model = ViewModel()
+
+    init() {
+        model.onTabTappedAgain = { [self] in
+            
+        }
+    }
+    
     var body: some View {
-        TabView {
-            WebView(url: URL(string: "https://tkn.eth.limo")!)
+        TabView(selection: $selectedTab.onUpdate{ model.tabTapped(item: selectedTab) }) {
+            WebView(url: URL(string: Constants.tab0)!, tag: 0)
                 .navigationBarHidden(true)
-                .edgesIgnoringSafeArea(.all)
+                .edgesIgnoringSafeArea(.horizontal)
                 .tabItem {
-                    Label("Tokens", systemImage: "list.bullet.rectangle")
+                    TabBarItem(title: "Tokens", image: svgImage(named: "list"))
                 }
                 .tag(0)
-            WebView(url: URL(string: "https://tkn.eth.limo/search")!)
+            WebView(url: URL(string: Constants.tab1)!, tag: 1)
                 .navigationBarHidden(true)
-                .edgesIgnoringSafeArea(.all)
+                .edgesIgnoringSafeArea(.horizontal)
                 .tabItem {
-                    Label("Search", systemImage: "magnifyingglass.circle.fill")
+                    TabBarItem(title: "Edit", image: svgImage(named: "edit"))
                 }
                 .tag(1)
-            WebView(url: URL(string: "https://tkn.eth.limo?m")!)
+            SearchView()
+                .navigationBarHidden(true)
+                .edgesIgnoringSafeArea(.horizontal)
+                .tabItem {
+                    TabBarItem(title: "Search", image: svgImage(named: "search"))
+                }
+                .tag(2)
+            WebView(url: URL(string: Constants.tab3)!, tag: 3)
                 .navigationBarHidden(true)
                 .edgesIgnoringSafeArea(.all)
                 .tabItem {
-                    Label("Portfolio", systemImage: "star.square")
+                    TabBarItem(title: "Portfolio", image: svgImage(named: "star"))
                 }
-                .tag(2)
+                .tag(3)
         }
+    }
+    
+    private func updateWebViewKey() {
+        print("Tab tapped again \(selectedTab)")
+    }
+}
+
+extension Binding {
+    func onUpdate(_ closure: @escaping () -> Void) -> Binding<Value> {
+        Binding(get: {
+            wrappedValue
+        }, set: { newValue in
+            wrappedValue = newValue
+            closure()
+        })
+    }
+}
+
+class ViewModel: ObservableObject {
+    @Published private(set) var lastTappedTab: Int?
+    
+    var onTabTappedAgain: (() -> Void)?
+
+    func tabTapped(item: Int) {
+        if lastTappedTab == item {
+            print("Tab tapped again with tag: \(item)")
+            NotificationCenter.default.post(name: NSNotification.Name("com.user.webView.reload"), object: nil, userInfo: ["selectedTab": item])
+            onTabTappedAgain?()
+        }
+        lastTappedTab = item
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+func svgImage(named name: String) -> Image {
+    if let url = Bundle.main.url(forResource: name, withExtension: "svg"),
+       let data = try? Data(contentsOf: url),
+       let image = SVGKImage(data: data),
+       let uiImage = image.uiImage {
+        return Image(uiImage: uiImage)
+    } else {
+        return Image(systemName: "exclamationmark.triangle")
+    }
+}
+
+struct TabBarItem: View {
+    let title: String
+    let image: Image
+    
+    var body: some View {
+        VStack {
+            image
+            Text(title)
+        }
     }
 }
